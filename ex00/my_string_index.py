4340a74910f978f63ef3b7437daa05bcbaa1e70a
fomegne meudje clark brady <clark-br_f@qwasar.io>
2025-05-15 21:15:31 +0000

    fix

--- ex00/my_string_index.py
+++ ex00/my_string_index.py
@@ -4,4 +4,8 @@
 :rtype: integer
 """
 def my_string_index(param_1, param_2):
-    return param_1.index(param_2)
+    for i in range(len(param_1)):
+        if param_1[i] == param_2:
+            return i
+    
+    return 0
