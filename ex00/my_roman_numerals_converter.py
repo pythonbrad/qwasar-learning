48a40eed6e98d9ec2f17d06581333201ce9ef0b7
fomegne meudje clark brady <clark-br_f@qwasar.io>
2025-05-16 23:09:08 +0000

    fix gandalf

--- ex00/my_roman_numerals_converter.py
+++ ex00/my_roman_numerals_converter.py
@@ -1,27 +1,24 @@
 import sys
 
 
-if len(sys.argv) < 2:
-    exit(0)
+def my_roman_numerals_converter(value):
+    out = ""
+    symbols = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
+    text = str(value)
+    n = len(text)
 
-text = sys.argv[1]
-out = ""
-n = len(text)
+    for i in range(n):
+        p = (n - i - 1) * 2
+        
+        d = int(text[i])
 
-symbols = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
+        if d < 4:
+            out += d * symbols[p]
+        elif d == 4:
+            out += symbols[p] + symbols[p+1]
+        elif d < 9:
+            out += symbols[p+1] + (d - 5) * symbols[p]
+        elif d == 9:
+            out += symbols[p] + symbols[p+2]
 
-for i in range(n):
-    p = (n - i - 1) * 2
-    
-    d = int(text[i])
-
-    if d < 4:
-        out += d * symbols[p]
-    elif d == 4:
-        out += symbols[p] + symbols[p+1]
-    elif d < 9:
-        out += symbols[p+1] + (d - 5) * symbols[p]
-    elif d == 9:
-        out += symbols[p] + symbols[p+1]
-
-print(out)
+    return out
