695b060eec7821822f38f8c7062f5422a14bbb30
Brady Fomegne <brady.fomegne@outlook.com>
2025-05-16 09:48:15 +0100

    fix mistake in ex04

--- ex04/my_is_sort.py
+++ ex04/my_is_sort.py
@@ -11,7 +11,6 @@ def my_is_sort(param_1):
     asc = 1 if param_1[0] < param_1[-1] else 0
 
     for i in range(size-1):
-        print(asc, param_1[i], param_1[i+1])
         if param_1[i] > param_1[i+1] == asc:
             return 0
 
