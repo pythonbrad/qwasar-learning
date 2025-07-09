b98fa2d78f319e5bd9458aa6e908c095bc4ced34
Brady Fomegne <brady.fomegne@outlook.com>
2025-07-09 18:06:30 +0100

    wip 9
    During the test of the picture searching, i got an "Internal Error".
    I believe this error come from the java code.
    After a debugging, the respective classes (create, delete, search) are not found.
    
    I have not yet tested but i hope this fix will work.

--- variables.tf
+++ variables.tf
@@ -2,3 +2,7 @@ variable "root_name" {
   description = "The root name' for your resources, such as S3 bucket"
   default     = "finala"
 }
+
+variable "java_home" {
+  description = "A path to JAVA 8"
+}
